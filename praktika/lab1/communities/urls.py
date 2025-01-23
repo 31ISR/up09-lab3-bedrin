2ce4aae38de1398552cb32189d23a1b655c464c0
Vanya <cocapula@gmail.com>
2025-01-23 15:25:25 +0600

    Revert "Dev"

--- praktika/lab1/communities/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin 
-from django.urls import path
-from . import views
-
-app_name = 'communities'
-
-urlpatterns = [
-    path('', views.communities_list, name='list'),
-]
